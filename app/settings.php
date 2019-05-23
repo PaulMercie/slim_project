d996cf288b58ca071fc74f48e63301f5411421ee
PaulMercie <paulmercie453@gmail.com>
2019-05-23 14:11:30 +0200

    :tada: Setup of the routing and files

---
+++ app/settings.php
@@ -0,0 +1,14 @@
+<?php
+
+$settings = [];
+
+// Display errors
+$settings['displayErrorDetails'] = true;
+
+// Database
+$settings['db'] = [];
+$settings['db']['host'] = 'localhost';
+$settings['db']['port'] = '8889';
+$settings['db']['name'] = 'hetic_students';
+$settings['db']['user'] = 'root';
+$settings['db']['pass'] = 'root';
